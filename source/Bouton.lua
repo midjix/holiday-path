79788c5bd83fa0360c24c4d02ef0334ed586da0f
William baal <williamxp95@gmail.com>
2024-05-07 16:36:11 +0200

    création du construteur de la classe Bouton

--- source/Bouton.lua
+++ source/Bouton.lua
@@ -5,3 +5,18 @@ import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+
+class('Bouton').extends(gfx.sprites)
+
+function Bouton:init(x, y, imgNormal, imgSelect, imgPress)
+    Bouton.super.init(self)
+    self:moveTo(x, y)
+    self.imgNormal = gfx.image.new(imgNormal)
+    self:setImage(self.imgNormal)
+    self.imgSelect = gfx.image.new(imgSelect)
+    self.imgPress = gfx.image.new(imgPress)
+end
+
+function Bouton:update(state)
+    
+end
