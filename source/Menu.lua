c9c9ea8fb143eb328d1fadf8a9ebf3abf80e1a6d
William baal <williamxp95@gmail.com>
2024-05-07 18:15:45 +0200

    mise en forme de la class Menu

--- source/Menu.lua
+++ source/Menu.lua
@@ -5,3 +5,13 @@ import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+
+class('Menu').extends(gfx.sprite)
+
+function Menu:init()
+    
+end
+
+function Menu:update()
+    
+end
