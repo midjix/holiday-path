69d5b4157358270503d7fceeb8210ab761efe6c2
William baal <williamxp95@gmail.com>
2024-05-07 15:55:56 +0200

    teste de la fonction playdate.update

--- source/main.lua
+++ source/main.lua
@@ -1,3 +1,4 @@
+-- par défaut le jeux est a 30 fps
 import "CoreLibs/graphics"
 import "CoreLibs/object"
 import "CoreLibs/sprites"
@@ -5,9 +6,13 @@ import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+local cercleRayon = 10
 
-gfx.fillCircleAtPoint(200, 120, 10)
+playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
 
-function playdate.update()
+function playdate.update() -- se lance a chaque frame
 	
+	cercleRayon += 1
+	gfx.fillCircleAtPoint(200, 120, cercleRayon)
+	playdate.drawFPS(0,0)
 end
