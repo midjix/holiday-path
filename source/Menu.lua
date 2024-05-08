8deb012f83b1b3b6bd42d0a11cd1b3f5678c2ed5
William baal <williamxp95@gmail.com>
2024-05-08 17:29:50 +0200

    mise a jour de la class Menu

--- source/Menu.lua
+++ source/Menu.lua
@@ -11,30 +11,65 @@ class('Menu').extends(gfx.sprite)
 
 function Menu:init()
     Menu.super.init(self)
-    self.bontonTest1 = Bouton(100, 120, "images/normal", "images/select", "images/press")
-	self.bontonTest1.etat = 1
-	self.bontonTest2 = Bouton(300, 120, "images/normal", "images/select", "images/press")
+
+	self.boutons = {} --tableau de boutons
+	self.boutons[1] = {}
+	self.boutons[2] = {}
+    self.boutons[1][1] = Bouton(200, 120, "images/normal", "images/select", "images/press") -- Bouton play
+	self.boutons[1][1].etat = 0
+	self.boutons[2][1] = Bouton(133, 180, "images/normal", "images/select", "images/press") -- Bouton Crédit
+	self.boutons[2][1].etat = 0
+	self.boutons[2][2] = Bouton(266, 180, "images/normal", "images/select", "images/press") -- Bonton règle
+	self.boutons[2][2].etat = 0
+
+	self.boutonSelect = {2, 1} -- Enregistre les indices du bouton selectionné 
+	print(self.boutonSelect[1] + 1)
+	print(self.boutons[2][2])
+	
     self:add()
 end
 
 function Menu:selection()
+	self.boutons[self.boutonSelect[1]][self.boutonSelect[2]].etat = 0 -- change l'etat de l'encien boutons selectionné à Normal
+	
+	-- selectionne le bonton de gauche si le bonton exist
 	if pd.buttonIsPressed(pd.kButtonLeft) then
-        self.bontonTest1.etat = 1
-		self.bontonTest2.etat = 0
+		if   self.boutons[self.boutonSelect[1]][self.boutonSelect[2] - 1] ~= nil then
+			self.boutonSelect[2] -= 1 
+		end
 	end
+
+	-- selectionne le bonton de droit si le bonton exist
 	if pd.buttonIsPressed(pd.kButtonRight) then
-		self.bontonTest1.etat = 0
-		self.bontonTest2.etat = 1
+		if  self.boutons[self.boutonSelect[1]][self.boutonSelect[2] + 1] ~= nil then
+			self.boutonSelect[2] += 1 
+		end
 	end
+
+	-- selectionne le bonton du haut si le bonton exist
+	if pd.buttonIsPressed(pd.kButtonUp) then
+		if self.boutons[self.boutonSelect[1] - 1][self.boutonSelect[2]] ~= nil then
+			self.boutonSelect[1] -= 1 
+		end
+	end
+
+	-- selectionne le bonton du bas si le bonton exist
+	if pd.buttonIsPressed(pd.kButtonDown) then
+		if self.boutons[self.boutonSelect[1] + 1][self.boutonSelect[2]] ~= nil then
+			self.boutonSelect[1] += 1 
+		end
+	end
+	
+	self.boutons[self.boutonSelect[1]][self.boutonSelect[2]].etat = 1 -- change l'etat du nouveau boutons selectionné à Select
 end
 
 function Menu:presserBouton()
-    if self.bontonTest1.etat == 1 and pd.buttonIsPressed(pd.kButtonA) then
-        self.bontonTest1.etat = 2
+    if self.boutons[1][1].etat == 1 and pd.buttonIsPressed(pd.kButtonA) then
+        self.boutons[1][1].etat = 2
 	end
 
-	if self.bontonTest2.etat == 1 and pd.buttonIsPressed(pd.kButtonA) then
-        self.bontonTest2.etat = 2
+	if self.boutons[2][1].etat == 1 and pd.buttonIsPressed(pd.kButtonA) then
+        self.boutons[2][1].etat = 2
 	end
     
 end
