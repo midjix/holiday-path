634770aa8e4bb4c4df2bca35744ca8dcb7dc3c9d
William Baal <williamxp95@gmail.com>
2024-06-06 11:57:13 +0200

    mise en page Menu:init()

--- source/Menu.lua
+++ source/Menu.lua
@@ -14,15 +14,21 @@ function Menu:init()
     Menu.super.init(self)
 
 	self.boutons = {} --tableau de boutons
+
     self.boutons[1*10 + 1] = Bouton(133, 120, "images/PLAY neutre", "images/PLAY Select", "images/PLAY press") -- Bouton Play
 	self.boutons[1*10 + 1].etat = 0
+
 	self.boutons[1*10 + 2] = Bouton(266, 120, "images/EXIT neutre", "images/EXIT Select", "images/EXIT press") -- Bouton Exit
 	self.boutons[1*10 + 2].etat = 0
+
 	self.boutons[2*10 + 1] = Bouton(70, 220, "images/Lab-BBG neutre", "images/Lab-BBG Select", "images/Lab-BBG press") -- Bouton Crédit
 	self.boutons[2*10 + 1].etat = 0
+
 	self.boutons[2*10 + 2] = Bouton(330, 220, "images/RULE neutre", "images/RULE Select", "images/RULE press") -- Bonton Règle
 	self.boutons[2*10 + 2].etat = 0
+
 	self.boutonSelect = {1, 1} -- Enregistre les indices du bouton selectionné 
+	
 	self.estVisible = true
     self:add()
 end
